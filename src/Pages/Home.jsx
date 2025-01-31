e9c3950c3f26cedc0b9b1b43872386c9efe95f8d
Prashant <mprashant9451@gmail.com>
2025-01-31 12:35:06 +0530

    Added pizza images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import IconArrowRight from "../Components/icons/IconArrowRight";
 
+import PizzaImage from '../assets/Images/pizza2.png';
+
 function Home() {
     return (
         <div>
             {/*Hero Section*/}
             <section 
                 className="flex flex-col-reverse items-center justify-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300"
-                >
+            >
 
                     <div className="w-4/6 ml-4 text-center md:w-2/6 md:text-left">
 
@@ -39,6 +41,15 @@ function Home() {
 
 
                         </button>
+                    </div>
+
+                    <div>
+                        <img
+                            src={PizzaImage}
+                            alt="Pizza"
+                            width={550}
+                            height={550}
+                        />
                     </div>
 
             </section>
