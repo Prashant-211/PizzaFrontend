176667132f47f72658504e7cdf7dc8f14a531f92
Prashant <mprashant9451@gmail.com>
2025-02-03 00:52:30 +0530

    Added signup page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,8 +5,10 @@ import PizzaImage from '../assets/Images/pizza2.png';
 import OrderFood from '../assets/Images/orderFood.png';
 import Pickup from '../assets/Images/pickup.png';
 import Enjoy from '../assets/Images/enjoy.png';
+import Layout from "../Layouts/Layout";
 function Home() {
     return (
+        <Layout>
         <div>
             {/*Hero Section*/}
             <section 
@@ -161,7 +163,7 @@ function Home() {
                 </div>   
             </section>
         </div>
-
+        </Layout>
     );
 }
 
